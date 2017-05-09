f9f292f9d2c59af648c2d9485f1c9db6c0c52c1d
ladylourdes <ladygrandaz@hotmail.es>
2017-05-08 20:01:30 -0500

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Institucion;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 
+ */
+public class Persona {
+     private Date nombre;
+     private Date direccion;
+     private Date telefono;
+     private Date email;
+     
+     
+     
+    
+}
